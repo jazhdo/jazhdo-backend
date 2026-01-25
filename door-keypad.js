94f50827bb110e2097248beb1390a4d7d42fbb15
Jazhdo <jaysonzdong@icloud.com>
2026-01-24 17:51:54 -0800

    Remove excessive console logging

--- door-keypad.js
+++ door-keypad.js
@@ -131,13 +131,13 @@ process.on('SIGINT', () => {
 while (true) {
     let key = null;
     for (let ci = 0; ci < cols.length; ci++) {
-        console.log(`Scanning col ${ci} (pin ${cols[ci]})`);
+        // console.log(`Scanning col ${ci} (pin ${cols[ci]})`);
         gpiox.set_gpio(cols[ci], 1);
         await sleep(1);
         
         for (let ri = 0; ri < rows.length; ri++) {
             const value = gpiox.get_gpio(rows[ri]);
-            console.log(`  Row ${ri} (pin ${rows[ri]}): ${value}`);
+            // console.log(`  Row ${ri} (pin ${rows[ri]}): ${value}`);
             
             if (value === 1) {
                 key = keys[ri][ci];
