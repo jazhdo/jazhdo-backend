49e35399113d1d6d56a66e8285f800e5b7726cf2
Jazhdo <jaysonzdong@icloud.com>
2026-01-27 19:00:21 -0800

    Changes to attempt to fix log file

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function active(url) {
         return response.ok
     } catch { return false }
 }
-function logFile(text) { fs.appendFile(`/home/raspberrypi/jazhdo-backend-logs/main/log_${startTime}.txt`, `[${Date.now()}] ${text}`, () => {}); }
+function logFile(text) { fs.appendFile(`/home/raspberrypi/jazhdo-backend-logs/main/log_${startTime}.txt`, `[${Date.now()}] ${text}`, (err) => { if (err) { console.log('Error logging:', err)} }); }
 function userDetails(req) { return [req.socket.remoteAddress, UAParser(req.headers['user-agent'])] }
 function basicDetails(user) { return `IP: ${user[0]}\nBrowser: ${user[1].browser.name} version ${user[1].browser.version}\nDevice: ${user[1].device.vendor} ${user[1].device.model}\nOS: ${user[1].os.name} version ${user[1].os.version}` }
 
@@ -43,6 +43,7 @@ const server = http.createServer(async (req, res) => {
             console.log(`Request to ${req.url} directed to Error 404`);
             res.statusCode = 404;
         } else if (!status) {
+            let user = userDetails(req);
             logFile(`Attempted reguest to access offline ${target} through url ${req.url} failed.\n${basicDetails(user)}`);
             console.log(`Unable to send ${req.url} directed to Error 503 (${target} offline)`);
             res.statusCode = 503;
