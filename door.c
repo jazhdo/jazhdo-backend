bc543d3cfc9b6ca56d49a48b34b250c2e22ca41f
Jazhdo <jaysonzdong@icloud.com>
2026-04-04 19:03:38 -0700

    fix

--- door.c
+++ door.c
@@ -4,73 +4,89 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 
 #define PORT 8080
-#define WIDTH 2304
-#define HEIGHT 1296
-#define FPS 30
-#define BOUNDARY "frame"
+#define BOUNDARY "jpgboundary"
+#define BUFSIZE (1024 * 1024)
 
 FILE *open_stream(void) {
-    FILE *pipe = popen("rpicam-vid -t 0 --width 2304 --height 1296 --framerate 30 --codec mjpeg --nopreview -o -", "r");
-    if (!pipe) {
-        fprintf(stderr, "failed to open rpicam-vid\n");
-        exit(1);
-    }
+    FILE *pipe = popen(
+        "rpicam-vid -t 0"
+        " --width 1536 --height 864"
+        " --framerate 30"
+        " --codec mjpeg"
+        " --nopreview"
+        " -o -",
+        "r"
+    );
+    if (!pipe) { fprintf(stderr, "failed to open rpicam-vid\n"); exit(1); }
     return pipe;
 }
 
 int start_server(void) {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        perror("socket");
-        exit(1);
-    }
-
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) { perror("socket"); exit(1); }
     int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        perror("bind");
-        exit(1);
-    }
-    if (listen(server_fd, 1) < 0) {
-        perror("listen");
-        exit(1);
-    }
-
-    printf("server listening on http://0.0.0.0:%d\n", PORT);
-    return server_fd;
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) { perror("bind"); exit(1); }
+    if (listen(fd, 5) < 0) { perror("listen"); exit(1); }
+    printf("http://0.0.0.0:%d\n", PORT);
+    return fd;
 }
 
 int main(void) {
     FILE *cam = open_stream();
     int server_fd = start_server();
 
-    /* read jpeg frames from rpicam-vid and serve over http */
     while (1) {
-        /* wait for a browser to connect */
         int client_fd = accept(server_fd, NULL, NULL);
-        if (client_fd < 0) {
-            perror("accept");
+        if (client_fd < 0) { perror("accept"); continue; }
+
+        int flag = 1;
+        setsockopt(client_fd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
+
+        char req[1024] = {0};
+        read(client_fd, req, sizeof(req) - 1);
+
+        if (strstr(req, "GET / ")) {
+            dprintf(client_fd,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/html\r\n\r\n"
+                "<html><body style='margin:0'>"
+                "<img src='/stream' style='width:100%%'>"
+                "</body></html>\r\n");
+            close(client_fd);
             continue;
         }
 
-        /* send HTTP headers for MJPEG stream */
-        dprintf(client_fd, "HTTP/1.1 200 OK\r\nContent-Type: multipart/x-mixed-replace; boundary=%s\r\n\r\n", BOUNDARY);
+        /* stream endpoint */
+        dprintf(client_fd,
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: multipart/x-mixed-replace; boundary=%s\r\n"
+            "Cache-Control: no-cache\r\n\r\n", BOUNDARY);
 
-        /* stream frames */
-        unsigned char buf[65536];
-        size_t n;
-        while ((n = fread(buf, 1, sizeof(buf), cam)) > 0) {
-            dprintf(client_fd, "--%s\r\nContent-Type: image/jpeg\r\nContent-Length: %zu\r\n\r\n", BOUNDARY, n);
-            write(client_fd, buf, n);
-            dprintf(client_fd, "\r\n");
+        static unsigned char frame[BUFSIZE];
+        int len = 0;
+        int c, prev = 0;
+
+        while ((c = fgetc(cam)) != EOF) {
+            if (len < BUFSIZE) frame[len++] = c;
+            if (prev == 0xFF && c == 0xD9) {
+                dprintf(client_fd,
+                    "--%s\r\n"
+                    "Content-Type: image/jpeg\r\n"
+                    "Content-Length: %d\r\n\r\n",
+                    BOUNDARY, len);
+                if (write(client_fd, frame, len) < 0) break;
+                dprintf(client_fd, "\r\n");
+                len = 0;
+            }
+            prev = c;
         }
 
         close(client_fd);
